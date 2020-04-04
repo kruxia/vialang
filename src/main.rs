ab4ef906cbf61c8ab5cb6056ea941fca43f2a8d3
Sean Harrison <sah@bookgenesis.com>
2020-04-04 12:32:08 -0500

    calculator 4 (commented out calculator 1–3 tests because of switching from raw Expr to boxed Expr

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,44 @@
 extern crate lalrpop_util;
 
 lalrpop_mod!(pub calculator); // synthesized by LALRPOP
+pub mod ast;
+
+// #[test]
+// fn calculator1() {
+//     assert!(calculator::TermParser::new().parse("22").is_ok());
+//     assert!(calculator::TermParser::new().parse("(22)").is_ok());
+//     assert!(calculator::TermParser::new().parse("((((22))))").is_ok());
+//     assert!(calculator::TermParser::new().parse("((22)").is_err());
+// }
+
+// #[cfg_attr(not(test), allow(unused_macros))]
+// macro_rules! test3 {
+//     ($expr:expr) => {
+//         println!("parsing {}", stringify!($expr));
+//         assert_eq!(
+//             calculator::ExprParser::new()
+//                 .parse(stringify!($expr))
+//                 .unwrap(),
+//             $expr
+//         );
+//     };
+// }
+
+// #[test]
+// fn calculator3() {
+//     test3!(22 + 44);
+//     test3!(22 - 44 - 66);
+//     test3!(22 * 44 + 66);
+//     test3!(22 * 44 + 66 / 3);
+//     test3!(22 * (44 + 66) / 3);
+// }
 
 #[test]
-fn calculator() {
-    assert!(calculator::TermParser::new().parse("22").is_ok());
-    assert!(calculator::TermParser::new().parse("(22)").is_ok());
-    assert!(calculator::TermParser::new().parse("((((22))))").is_ok());
-    assert!(calculator::TermParser::new().parse("((22)").is_err());
+fn calculator4() {
+    let expr = calculator::ExprParser::new()
+        .parse("22 * 44 + 66")
+        .unwrap();
+    assert_eq!(&format!("{:?}", expr), "((22 * 44) + 66)");
 }
 
 #[cfg(not(test))]
