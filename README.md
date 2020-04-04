bf9921707ae11c292c4f0120d925c1e61106a435
Sean Harrison <sah@bookgenesis.com>
2020-04-04 11:43:31 -0500

    calculator 1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # vialang
+
 Via Language
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#[macro_use]
+extern crate lalrpop_util;
+
+lalrpop_mod!(pub calculator); // synthesized by LALRPOP
+
+#[test]
+fn calculator() {
+    assert!(calculator::TermParser::new().parse("22").is_ok());
+    assert!(calculator::TermParser::new().parse("(22)").is_ok());
+    assert!(calculator::TermParser::new().parse("((((22))))").is_ok());
+    assert!(calculator::TermParser::new().parse("((22)").is_err());
+}
+
+#[cfg(not(test))]
 fn main() {
-    println!("Hello, world!");
 }
