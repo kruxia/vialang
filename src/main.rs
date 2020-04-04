8149d7074115bcceac2fb18f72c0353473099269
Sean Harrison <sah@bookgenesis.com>
2020-04-04 14:30:51 -0500

    initial tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#[macro_use]
+extern crate lalrpop_util;
+
+lalrpop_mod!(pub via); // synthesized by LALRPOP
+
+#[cfg(not(test))]
 fn main() {
     println!("Hello, world!");
+}
+
+#[test]
+fn via() {
+    assert!(via::TermParser::new().parse("42 hi this_is_ok so-is-this _and this_").is_ok());
 }
